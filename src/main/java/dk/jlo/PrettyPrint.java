5354f839a9f84629127fe891a8fb5c2c28239063
Jens Lohmann - Nine A/S <jens.lohmann@ext.cph.dk>
2020-02-27 11:26:04 +0100

    Updated to GraalVM v/20. A few maven plugin upgrades.

--- src/main/java/dk/jlo/PrettyPrint.java
+++ src/main/java/dk/jlo/PrettyPrint.java
@@ -17,12 +17,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class PrettyPrint {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charset.forName("UTF-8")));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
         prettify(reader, System.out, 4);
     }
